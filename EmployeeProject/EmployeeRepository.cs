16ab11be2f9a629d291a5e8ea72297e37643bf72
Warren Pitterson <warren.pitterson@kaleidaltd.co.uk>
2019-06-28 15:01:38 +0100

    Add Employee Repo class

--- EmployeeProject/EmployeeRepository.cs
+++ EmployeeProject/EmployeeRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +10,156 @@ namespace EmployeeProject
 {
     public class EmployeeRepository 
     {
+        public static string databasePath = ConfigurationManager.AppSettings["CsvDatabasePath"];
+        private readonly List<Employee> employees = new List<Employee>();
 
-    
+        public static void ManualAdd(List<Employee> employees)
+        {
+            try
+            {
+                string databasePath = ConfigurationManager.AppSettings["CsvDatabasePath"];
+                {
+                    using (var writer = new StreamWriter(databasePath, true))
+                    {
+                        Console.WriteLine("Employee ID");
+                        var EmployeeID = (Console.ReadLine());
+
+                        Console.WriteLine("Please enter first name");
+                        var firstName = Console.ReadLine();
+
+                        Console.WriteLine("Please enter last name");
+                        var lastName = Console.ReadLine();
+
+                        Console.WriteLine("Please enter date of birth (DD/MM/YYYY)");
+                        var dob = Console.ReadLine();
+                        DateTime.TryParse(dob, out DateTime parsedDob);
+
+                        Console.WriteLine("Please enter the employee start date (DD/MM/YYYY)");
+                        var startDate = Console.ReadLine();
+                        DateTime.TryParse(startDate, out DateTime parsedStartDate);
+
+                        Console.WriteLine("Please enter the employee hometown");
+                        var homeTown = Console.ReadLine();
+
+                        Console.WriteLine("Please enter the employee department");
+                        var department = Console.ReadLine();
+
+                        Employee newEmployee = new Employee(EmployeeID, firstName, lastName, parsedDob, parsedStartDate, homeTown, department);
+
+                        string result = $"{EmployeeID},{firstName},{lastName},{parsedDob}, {parsedStartDate},{homeTown},{department}";
+
+
+                        employees.Add(newEmployee);
+                        writer.WriteLine(result);
+                        writer.Close();
+                        Program.StartMenu(employees);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to add employee");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void AddEmployeeViaCSV(List<Employee> employees)
+        {
+            try
+            {
+                var databasePath = ConfigurationManager.AppSettings["CsvDatabasePath"];
+
+
+                using (var reader = new StreamReader(databasePath, true))
+
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+
+                        string employeeID = values[0];
+                        string firstName = values[1];
+                        string lastName = values[2];
+                        string dob = values[3];
+                        DateTime.TryParse(dob, out DateTime parsedDob);
+                        string StartDate = values[4];
+                        DateTime.TryParse(StartDate, out DateTime parsedStartDate);
+                        string hometown = values[5];
+                        string department = values[6];
+
+
+                        Employee newEmployee = new Employee(employeeID, firstName, lastName, parsedDob, parsedStartDate, hometown, department);
+
+                        employees.Add(newEmployee);
+                    }
+                    Console.WriteLine("Employees added from CSV\n");
+                }
+                Program.StartMenu(employees);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error Loading List");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
+        public static void RemoveEmployee(List<Employee> employees)
+        {
+            var databasePath = ConfigurationManager.AppSettings["CsvDatabasePath"];
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine("Please enter the employee number you wish to remove");
+            var employeeRemoved = Console.ReadLine();
+
+            foreach (var e in employees.ToList().Where(e => e.EmployeeID == employeeRemoved))
+            {
+                Console.WriteLine($"{e.FirstName} has been removed");
+                employees.Remove(e);
+            }
+
+            foreach (var e in employees)
+            {
+                string result = $"{e.EmployeeID},{e.FirstName},{e.LastName},{e.Dob}, {e.StartDate},{e.HomeTown},{e.Department}";
+                sb.AppendLine(result);
+            }
+
+            string contents = sb.ToString();
+
+            File.WriteAllText(databasePath, contents);
+
+            Program.StartMenu(employees);
+        }
+
+        public static void EditEmployee(List<Employee> employees)
+        {
+            var databasePath = ConfigurationManager.AppSettings["CsvDatabasePath"];
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine("Select the employee  number you wish to edit");
+            var employeeEdited = Console.ReadLine();
+
+            Console.WriteLine($"What would you like to change The Last Name to? ");
+            var newLastName = Console.ReadLine();
+
+            foreach (var e in employees.Where(e => e.EmployeeID == employeeEdited))
+            {
+                employees.Select(newlastName => { e.LastName = newLastName; return newlastName; }).ToList();
+                Console.WriteLine($"Name amended for {e.FirstName}");
+            }
+
+            foreach (var e in employees)
+            {
+                string newresult = $"{e.EmployeeID},{e.FirstName},{newLastName},{e.Dob}, {e.StartDate},{e.HomeTown},{e.Department}";
+                sb.AppendLine(newresult);
+            }
+
+            string contents = sb.ToString();
+            File.WriteAllText(databasePath, contents);
+
+            Program.StartMenu(employees);
+        }
     }
 }
--- EmployeeProject/Program.cs
+++ EmployeeProject/Program.cs
@@ -12,14 +12,16 @@ namespace EmployeeProject
 {
     class Program
     {
+        public static string databasePath = ConfigurationManager.AppSettings["CsvDatabasePath"];
+        public static List<Employee> employees = new List<Employee>();
+        public static EmployeeRepository EmployeeRepo = new EmployeeRepository();
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Program");
-            List<Employee> employees = new List<Employee>();
             StartMenu(employees);
+
         }
-        
 
         public static void StartMenu(List<Employee> employees)
         {
@@ -47,19 +49,20 @@ namespace EmployeeProject
                     break;
 
                 case 1:
-                    ManualAdd(employees);
+                    EmployeeRepository.ManualAdd(employees);
                     break;
 
                 case 2:
-                    AddEmployeeViaCSV(employees);
+                    EmployeeRepository.AddEmployeeViaCSV(employees);
                     break;
 
                 case 3:
+
                     ShowAllEmployees(employees);
                     break;
 
                 case 4:
-                    RemoveEmployee(employees);
+                    EmployeeRepository.RemoveEmployee(employees);
                     break;
                 case 5:
                     EmployeeMonthlyAnniversary(employees);
@@ -74,7 +77,7 @@ namespace EmployeeProject
                     ShowAgeByDepartments(employees);
                     break;
                 case 9:
-                    EditEmployee(employees);
+                    EmployeeRepository.EditEmployee(employees);
                     break;
 
                 default:
@@ -82,102 +85,6 @@ namespace EmployeeProject
                     break;
             }
             StartMenu(employees);
-        }
-
-
-        public static void ManualAdd(List<Employee> employees)
-        {
-            try
-            {
-                string databasePath = ConfigurationManager.AppSettings["CsvDatabasePath"];
-
-                {
-                    using (var writer = new StreamWriter(databasePath, true))
-                    {
-                        Console.WriteLine("Employee ID");
-                        var EmployeeID = (Console.ReadLine());
-
-                        Console.WriteLine("Please enter first name");
-                        var firstName = Console.ReadLine();
-
-                        Console.WriteLine("Please enter last name");
-                        var lastName = Console.ReadLine();
-
-                        Console.WriteLine("Please enter date of birth (DD/MM/YYYY)");
-                        var dob = Console.ReadLine();
-                        DateTime.TryParse(dob, out DateTime parsedDob);
-
-                        Console.WriteLine("Please enter the employee start date (DD/MM/YYYY)");
-                        var startDate = Console.ReadLine();
-                        DateTime.TryParse(startDate, out DateTime parsedStartDate);
-
-                        Console.WriteLine("Please enter the employee hometown");
-                        var homeTown = Console.ReadLine();
-
-                        Console.WriteLine("Please enter the employee department");
-                        var department = Console.ReadLine();
-
-                        Employee newEmployee = new Employee(EmployeeID, firstName, lastName, parsedDob, parsedStartDate, homeTown, department);
-
-                        string result = $"{EmployeeID},{firstName},{lastName},{parsedDob}, {parsedStartDate},{homeTown},{department}";
-
-
-                        employees.Add(newEmployee);
-                        writer.WriteLine(result);
-                        writer.Close();
-                        StartMenu(employees);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unable to add employee");
-                Console.WriteLine(e.Message);
-            }
-        }
-
-
-        public static void AddEmployeeViaCSV(List<Employee> employees)
-        {
-            try
-            {
-                var databasePath = ConfigurationManager.AppSettings["CsvDatabasePath"];
-
-
-                using (var reader = new StreamReader(databasePath, true))
-
-                { 
-                    while (!reader.EndOfStream)
-                    {
-                        var line = reader.ReadLine();
-                        var values = line.Split(',');
-
-                        string employeeID = values[0];
-                        string firstName = values[1];
-                        string lastName = values[2];
-                        string dob = values[3];
-                        DateTime.TryParse(dob, out DateTime parsedDob);
-                        string StartDate = values[4];
-                        DateTime.TryParse(StartDate, out DateTime parsedStartDate);
-                        string hometown = values[5];
-                        string department = values[6];
-
-
-                        Employee newEmployee = new Employee(employeeID, firstName, lastName, parsedDob, parsedStartDate, hometown, department);
-
-                        employees.Add(newEmployee);
-                    }
-                    Console.WriteLine("Employees added from CSV\n");
-                }
-                StartMenu(employees);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error Loading List");
-                Console.WriteLine(e.Message);
-            }
-
-
         }
 
         public static void ShowAllEmployees(List<Employee> employees)
@@ -188,69 +95,11 @@ namespace EmployeeProject
             }
 
             StartMenu(employees);
-
-
-        }
-
-        public static void RemoveEmployee(List<Employee> employees)
-        {
-            var databasePath = ConfigurationManager.AppSettings["CsvDatabasePath"];
-            StringBuilder sb = new StringBuilder();
-
-            Console.WriteLine("Please enter the employee number you wish to remove");
-                var employeeRemoved = Console.ReadLine();
-
-            foreach (var e in employees.ToList().Where(e => e.EmployeeID == employeeRemoved)) 
-                {
-                    Console.WriteLine($"{e.FirstName} has been removed");
-                    employees.Remove(e);
-                }
-
-            foreach (var e in employees)
-            {
-                string result = $"{e.EmployeeID},{e.FirstName},{e.LastName},{e.Dob}, {e.StartDate},{e.HomeTown},{e.Department}";
-                sb.AppendLine(result);
-            }
-
-            string contents = sb.ToString();
-    
-            File.WriteAllText(databasePath, contents);
-
-            StartMenu(employees);
-        }
-
-        public static void EditEmployee(List<Employee> employees)
-        {
-            var databasePath = ConfigurationManager.AppSettings["CsvDatabasePath"];
-            StringBuilder sb = new StringBuilder();
-
-            Console.WriteLine("Select the employee  number you wish to edit");
-            var employeeEdited = Console.ReadLine();
-
-            Console.WriteLine($"What would you like to change The Last Name to? ");
-            var newLastName = Console.ReadLine();
-
-            foreach (var e in employees.Where(e => e.EmployeeID == employeeEdited))
-            {
-                employees.Select(newlastName => { e.LastName = newLastName; return newlastName; }).ToList();
-                Console.WriteLine($"Name amended for {e.FirstName}");
-            }
-
-            foreach (var e in employees)
-            {
-                string newresult = $"{e.EmployeeID},{e.FirstName},{newLastName},{e.Dob}, {e.StartDate},{e.HomeTown},{e.Department}";
-                sb.AppendLine(newresult);
-            }
-
-            string contents = sb.ToString();
-            File.WriteAllText(databasePath, contents);
-
-            StartMenu(employees);
         }
 
         public static void ShowAgeByDepartments(List<Employee> employees)
         {
-          foreach (var employeeGroup in employees.GroupBy (e=> e.Department))
+            foreach (var employeeGroup in employees.GroupBy(e => e.Department))
             {
                 Console.WriteLine($"{employeeGroup.Key}");
                 foreach (var e in employeeGroup)
@@ -291,14 +140,14 @@ namespace EmployeeProject
 
                 Console.WriteLine($"\t\tTotal age combined: {totalAge}");
                 Console.WriteLine($"\t\tStaff in Department: {people}");
-                Console.WriteLine($"\t\tAverage age of Staff in Department: {totalAge/people}");
+                Console.WriteLine($"\t\tAverage age of Staff in Department: {totalAge / people}");
                 Console.WriteLine("\n");
             }
         }
 
         static void NumberOfEmployeesByTown(List<Employee> employees)
         {
-          foreach (var h in employees.GroupBy(e => e.HomeTown))
+            foreach (var h in employees.GroupBy(e => e.HomeTown))
             {
                 var Count = 0;
                 Console.WriteLine($"{h.Key}");
@@ -313,18 +162,3 @@ namespace EmployeeProject
         }
     }
 }
-
-
-
-
-//private static void EditEmployee()
-//{
-//    Console.WriteLine("Enter the Id of the Employee whose details you wish to edit.");
-//    var input = Console.ReadLine();
-//    Int32.TryParse(input, out int Id);
-//    var x = Employees.FirstOrDefault(e => e.EmployeeId == Id);
-
-//    EmployeeRepo.UpdateEmployee(x);
-
-//    Console.WriteLine($"Employee {Id} has been edited.");
-//}
